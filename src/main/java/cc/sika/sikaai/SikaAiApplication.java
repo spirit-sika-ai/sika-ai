cf2362886dd5f4a299b1f99722d2f03f7c017aca
小吴来哩 <wuchang@gdcattsoft.com>
2025-03-07 09:34:47 +0800

    init

---
+++ src/main/java/cc/sika/sikaai/SikaAiApplication.java
@@ -0,0 +1,13 @@
+package cc.sika.sikaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikaAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SikaAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/sika/sikaai/SikaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.sika.sikaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SikaAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
